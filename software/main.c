b558483b81d269e2a60caf618e8f962780f229ff
Lauren <lauren@tomasello.me>
2013-05-05 22:04:59 +0100

    Some small bug fixes and improved comments.

--- software/main.c
+++ software/main.c
@@ -12,25 +12,35 @@ struct {
 	int last_index;
 } state;
 
-int get_channels() {
+inline int get_channels() {
 	return state.channels;
 }
 
+// Updates the state for which channels are active
+// Works out what intervals are necessary
 void set_channels(int new_channels) {
-	char highest_rate = 0;
+	char sampling_rate = 0;
 	char digits[3];
 	
+	// If any packets were received whilst new channels are being set, the data sent
+	// to the handel-c component would be sent without a preamble, so mac interrupts
+	// are temporarily disabled.
 	mac_disable_interrupts();
 
-	state.channels = new_channels;
-	set_leds(0xFF & (new_channels>>1));
+	state.channels = new_channels;      // Update state
+	set_leds(0xFF & (new_channels>>1)); // Update LEDs on the Spartan
 
 	uart_send_string(UART, "\r\n [Active channels:");
+	
+	// The highest rate that is a multiple of other rates must be found so that the sampling rate
+	// can be set to it.
 	for (int i=1; i<=20; i++) {
 		if (TEST_BIT(state.channels, i)) {
-			if (channels[i].rate > highest_rate) {
-				highest_rate = channels[i].rate;
+			if (channels[i].rate > sampling_rate) {
+				sampling_rate = channels[i].rate;
 			}
+			
+			// Whilst checking the sample rates, also display the newly active channels to the user.
 			uart_send_char(UART, ' ');
 			uart_send_string(UART, int2digit(i, digits));
 		}
@@ -38,12 +48,15 @@ void set_channels(int new_channels) {
 	uart_send_char(UART, ']');
 	
 	for (int i=1; i<=20; i++) {
-		channels[i].interval = 1 << (highest_rate - channels[i].rate);
+		// The interval to insert samples for this channel into the buffer depends on what the sampling rate
+		// will be set at.
+		channels[i].interval = 1 << (sampling_rate - channels[i].rate);
 	}
 	
-	// Reset the stream and set the buffer rate to the fastest
-	hc_reset_stream(highest_rate);
+	// Reset the stream and set the buffer rate to the fastest.
+	hc_reset_stream(sampling_rate);
 	
+	// Now packets can be safely forwarded to the handel-c component again.
 	mac_enable_interrupts();
 }
 
@@ -52,9 +65,11 @@ void inth_mac() {
 	while ((packet = mac_packet_ready())) {
 		short type = (short)((*(packet+3) & 0xFFFF0000) >> 16U);
 		
+		// Only handle packets with the correct type - all others should be ignored.
 		if (type == 0x55AA) {
 			short stream = (short)((*(packet+3) & 0x0000FFFF));
 			
+			// Only forward packets for activated channels.
 			if (TEST_BIT(state.channels, stream)) {
 				ChanInfo *channel = &channels[stream-1];
 				
@@ -64,13 +79,18 @@ void inth_mac() {
 				int index  = (int)(*(packet+5));
 				int length = (int)(*(packet+6));
 				
+				// The preamble must be sent to the handel-c component first, so that it knows where to
+				// position the samples in the buffer.
 				hc_preamble(channel->width, channel->interval, (signed)state.last_index - index, length);
 				
+				// Forward all packets to the handel-c component.
 				for (int i=0; i < length/4; i++) {
 					hc_put(*(packet+7+i));
 				}
 				
-				state.last_index = index + length;
+				// The position in the buffer where this packet ended must be stored so that the
+				// following packet can be inserted in the correct relative position.
+				state.last_index = index + (length * channel->interval);
 			}
 		}
 		
@@ -80,12 +100,15 @@ void inth_mac() {
 
 void inth_uart() {
 	while (uart_check_char(UART)) {
+		// Forward any console input to the GUI module.
 		gui_accept(uart_get_char(UART));
 	}
 }
 
 void inth_switches() {
+	// Set the active channel to be the switch just activated.
 	set_channels(1 << (get_switches()+1));
+	
 	switches_clear_interrupt();
 }
 
